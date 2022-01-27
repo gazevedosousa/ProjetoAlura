ea506395147da1ed6480ea618671f9a159e0b24d
unknown <gazevedosousa@hotmail.com>
2022-01-27 19:24:51 -0300

    aula 3 exercicio 6 em andamento

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller {
+
+    public function index (Request $request) {
+        $series = [
+            "Grey's Anatomy",
+            "Lost",
+            "Agentes of SHIELD"
+        ];
+
+        return view('series.index', compact('series'));
+    }
+
+    public function create() {
+
+        return view('series.create');
+    }
+
+}
