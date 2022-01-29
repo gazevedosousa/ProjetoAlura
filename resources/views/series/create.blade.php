4228998c0ee1c94ff85f2e3ca3f3346e12bcce04
unknown <gazevedosousa@hotmail.com>
2022-01-29 18:30:05 -0300

    começando aula 04 - parte 04 - migrations

---
+++ resources/views/series/create.blade.php
@@ -0,0 +1,20 @@
+@extends('layout')
+
+@section('cabecalho')
+
+    Adicionar Séries
+
+@endsection
+
+@section('conteudo')
+
+    <form method="post">
+        <div class="form-group">
+            <label for="nome">Nome</label>
+            <input type="text" class = "form-control" name="nome">
+        </div>
+
+        <button class="btn btn-primary m-2">Adicionar</button>
+    </form>
+
+@endsection
